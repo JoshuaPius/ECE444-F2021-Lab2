68d93ff22b51a70a71e331884ff198cf2a0f1f9d
Joshua Pius <piusjoshuaa@gmail.com>
2021-09-26 11:27:58 -0400

    Transferred my implementation of Activity 3 from user.html to index.html. This was a misunderstanding on my part

--- hello.py
+++ hello.py
@@ -9,11 +9,11 @@ Bootstrap(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html', name=name, current_time=datetime.utcnow())
+	return render_template('user.html', name=name)
 
 if __name__ == '__main__':
 	app.run(debug=True)
